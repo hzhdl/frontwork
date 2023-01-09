4806148b3ff230da1c63895ec672c61c413f6a23
ZFunnying <1060346850@qq.com>
2023-01-09 12:08:47 +0800

    搜索栏布局完成，功能未实现

--- src/views/edit_vfor.vue
+++ src/views/edit_vfor.vue
@@ -1,5 +1,48 @@
 <template>
+  <a-form ref="formRef" :model="form" label-width="100px">
+    <a-space>
+    <a-row :gutter="5">
+      SearchBy: </a-row>
+      <a-row :gutter="5">
+          <a-col :span="6.5">
+              <a-input
+              addon-before="name:"
+              ref="searchName"
+              ></a-input>
+          </a-col>
+          <a-col :span="6.5">
+              <a-input
+              addon-before="age:"
+              ref="searchAge"
+              ></a-input>
+          </a-col>
+          <a-col :span="6.5">
+              <a-input
+              addon-before="address:"
+              ref="searchAddress"
+              ></a-input>
+          </a-col>
+          <a-col>
+              <a-button
+              type="primary"
+              @click="search"
+              >
+              <template #icon><SearchOutlined /></template>
+              search</a-button>
+          </a-col>
+          <a-col>
+              <a-button
+              type="primary"
+              @click="add"
+              >
+              <template #icon><PlusCircleOutlined /></template>
+              add</a-button>
+          </a-col>
+    </a-row>
+    </a-space>
+  </a-form>
     <a-table :columns="columns" :data-source="dataSource" bordered>
+
       <template v-for="col in ['name', 'age', 'address']" #[col]="{ text, record }" :key="col">
         <div>
           <a-input
@@ -16,12 +59,17 @@
         <div class="editable-row-operations">
           <span v-if="editableData[record.key]">
             <a @click="save(record.key)">Save</a>
-            <a-popconfirm title="Sure to cancel?" @confirm="cancel(record.key)">
-              <a>Cancel</a>
-            </a-popconfirm>
+              <a @click="cancel(record.key)">Cancel</a>
           </span>
           <span v-else>
             <a @click="edit(record.key)">Edit</a>
+            <a-popconfirm
+             v-if="dataSource.length"
+             title="Sure to delete?"
+             @confirm="onDelete(record.key)"
+          >
+            <a>Delete</a>
+            </a-popconfirm>
           </span>
         </div>
       </template>
@@ -30,6 +78,8 @@
   <script>
   import { cloneDeep } from 'lodash-es';
   import { defineComponent, reactive, ref } from 'vue';
+  import { SearchOutlined, PlusCircleOutlined } from '@ant-design/icons-vue';
+
   const columns = [
     {
       title: 'name',
@@ -75,13 +125,34 @@
   }
   
   export default defineComponent({
+    components: {
+    SearchOutlined,
+    PlusCircleOutlined,
+  },
     setup() {
+      const formRef = ref()
+      const form = reactive(
+      {
+        name: '',
+        age: '',
+        address: ''
+      }
+    )
+
       const dataSource = ref(data);
       const editableData = reactive({});
+
+      const add = () => {
+        //editableData[key] = cloneDeep(dataSource.value.filter(item => key === item.key)[0]);
+      };
   
       const edit = key => {
         editableData[key] = cloneDeep(dataSource.value.filter(item => key === item.key)[0]);
       };
+
+      const onDelete = key => {
+      dataSource.value = dataSource.value.filter(item => item.key !== key);
+    };
   
       const save = key => {
         Object.assign(dataSource.value.filter(item => key === item.key)[0], editableData[key]);
@@ -97,6 +168,7 @@
         columns,
         editingKey: '',
         editableData,
+        onDelete,
         edit,
         save,
         cancel,
@@ -106,6 +178,9 @@
   </script>
   <style scoped>
   .editable-row-operations a {
+    margin-right: 8px;
+  }
+  .delete-row-operations a {
     margin-right: 8px;
   }
   </style>
